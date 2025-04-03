91d6536187d4b3bb8f13ec298bca9d85bf6b72e0
Jerome <robotique-brax@gsmll.com>
2025-04-03 16:59:06 +0200

    feat: initialisation de l'application Quasar

---
+++ src/pages/ErrorNotFound.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">
+        404
+      </div>
+
+      <div class="text-h2" style="opacity:.4">
+        Oops. Il n'y a rien ici...
+      </div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="blue"
+        unelevated
+        to="/"
+        label="Aller à l'accueil"
+        no-caps
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+//
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="flex flex-center">
+    Page d'accueil
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
