069745c03b1f30db49ea7528880230a234c91d06
mat <mathys.inard@yahoo.com>
2025-06-19 19:05:28 +0200

    création route vers la page de présentatation individuel des intervenants

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,10 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { name: 'home', path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'intervenant', component: () => import('pages/IntervenantPage.vue') },
+      { path: 'intervenant', component: () => import('src/pages/IntervenantsPage.vue') },
       { path: 'cycle', component: () => import('pages/CyclesPage.vue') },
       { path: 'cycle/:id', component: () => import('pages/CyclePage.vue') },
+      { path: 'intervenant/:id', component: () => import('pages/IntervenantPage.vue'), props: true },
     ],
   },
 
