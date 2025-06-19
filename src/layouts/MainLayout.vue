7e714036df3cdf2bf23e570a217181765f84c647
mat <mathys.inard@yahoo.com>
2025-06-19 19:04:33 +0200

    suppression des icons absent dans le header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,8 +4,8 @@
       <q-toolbar>
         <q-toolbar-title> Arts en peinture </q-toolbar-title>
         <q-btn flat @click="$router.push({ name: 'home' })" icon="mdi-home"></q-btn>
-        <q-btn flat @click="$router.push('/intervenant/')" icon="">intervenant</q-btn>
-        <q-btn flat @click="$router.push('/cycle/')" icon="">cycle</q-btn>
+        <q-btn flat @click="$router.push('/intervenant/')">intervenant</q-btn>
+        <q-btn flat @click="$router.push('/cycle/')">cycle</q-btn>
       </q-toolbar>
     </q-header>
 
