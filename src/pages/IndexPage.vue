6b7ae0caa47effa28ccb77166643d3f5cadba818
mat <mathys.inard@yahoo.com>
2025-06-19 19:56:34 +0200

    création page d'accueil

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,35 @@
 <template>
-  <q-page class="flex flex-center"> Page d'accueil </q-page>
+  <q-section class="flex flex-center présentation background">
+    <h1>Le cycle en cour est {{ firstCycle.title }}</h1>
+    <h3>
+      {{ firstCycle.description }}<br />
+      Animé par {{ firstCycle.teacher }}, du {{ firstCycle.startDate }} au {{ firstCycle.endDate }}<br />
+    </h3>
+  </q-section>
+  <q-section class="flex flex-center présentation background">
+    <h1>Qui somme nous ?</h1>
+    <h3>
+      Nous sommes une association rattachée au <a href="https://www.frbrax31.fr/page/963747-presentation" target="_blank" rel="noopener noreferrer">foyer rural de Brax</a>. Notre association fait
+      venir des intervenants dans le but d’animer des ateliers autour d’un thème.
+    </h3>
+  </q-section>
 </template>
 
 <script setup>
-//
+import { computed } from 'vue'
+import dataBase from 'src/data/database.json'
+
+const firstCycle = computed(() => {
+  return dataBase.cycles[0]
+})
 </script>
+
+<style>
+.background {
+  background-color: whitesmoke;
+}
+
+.présentation {
+  margin: 70px;
+}
+</style>
